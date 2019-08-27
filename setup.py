f5625b93d347da9a49aba19957117bfd266232de
eliad hershkovitz <eliad.hershkovitz@briefcam.com>
2019-08-27 19:20:47 +0300

    Update dependencies

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask', 'flask-shell-ipython',
+        'flask', 'flask-shell-ipython', 'flask-sqlalchemy', 'flask-wtf'
     ],
 )
